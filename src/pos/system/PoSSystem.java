36d0d35639bff33ac60d17790a450799b923c15c
janitha <janitha@janitha-e7480.internal.synopsys.com>
2018-11-26 23:20:00 +0530

    Init

--- src/pos/system/PoSSystem.java
+++ src/pos/system/PoSSystem.java
@@ -5,6 +5,8 @@
  */
 package pos.system;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author janitha
@@ -15,7 +17,10 @@ public class PoSSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here                
-    }
+            AddItem add = new AddItem();
+            add.setVisible(true);
+            
+        
+                }
     
 }
